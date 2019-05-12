551c7331633ef45756eda76430a69a66e3a58fad
YourMCGeek <6243879+YourMCGeek@users.noreply.github.com>
2019-05-12 11:09:39 -0400

    Continued work on Remove Command for Remind

--- src/main/java/com/github/yourmcgeek/shadowrewrite/commands/remind/Remove.java
+++ src/main/java/com/github/yourmcgeek/shadowrewrite/commands/remind/Remove.java
@@ -1,6 +1,8 @@
 package com.github.yourmcgeek.shadowrewrite.commands.remind;
 
+import com.github.yourmcgeek.shadowrewrite.EmbedTemplates;
 import com.github.yourmcgeek.shadowrewrite.ShadowRewrite;
+import com.github.yourmcgeek.shadowrewrite.storage.SQLManager;
 import me.bhop.bjdautilities.ReactionMenu;
 import me.bhop.bjdautilities.command.annotation.Command;
 import me.bhop.bjdautilities.command.annotation.Execute;
@@ -11,12 +13,14 @@ import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
-@Command(value = "remove", hideInHelp = true)
+@Command(value = "remove", hideInHelp = true, usage = "remind remove 1")
 public class Remove {
 
     private ShadowRewrite main;
+    private ArrayList<Reminder> remindersList;
 
     @Execute
     public CommandResult onRemove(Member member, TextChannel channel, Message message, String label, List<String> args, ShadowRewrite main) {
@@ -25,15 +29,24 @@ public class Remove {
                 .setColor(new Color(main.getConfig().getConfigValue("Red").getAsInt(), main.getConfig().getConfigValue("Blue").getAsInt(), main.getConfig().getConfigValue("Green").getAsInt()))
                 .setTitle("Active Reminders");
         for (Reminder reminder : main.getSqlManager().getRemindersForUser(member.getUser().getIdLong())) {
+            remindersList.add(reminder);
             builder.appendDescription(count + ". " + reminder.getMessage() + "\n");
             count++;
         }
+        main.getLogger().info(remindersList.toString());
+        if (args.isEmpty()) {
         ReactionMenu reactionMenu = new ReactionMenu.Builder(member.getJDA())
                 .addStartingReaction("\u274C")
                 .setEmbed(builder.build())
-                .onClick("\u274C", menu -> {
-                    menu.destroy();
-                }).buildAndDisplay(channel);
+                .onClick("\u274C", menu -> menu.destroy()).buildAndDisplay(channel);
+
+        main.getMessenger().sendEmbed(channel, EmbedTemplates.ERROR.getEmbed().setDescription("Invalid Arguments, run the command with the remind number you'd like to remove.").build(), 10);
+
+            return CommandResult.success();
+        }
+
+        main.getLogger().info("Removing Reminder: " + remindersList.get(Integer.valueOf(args.get(0)) - 1).getMessage() + "\n" + remindersList.get(Integer.valueOf(args.get(0))-1).getCreation());
+        main.getSqlManager().removeRemind(remindersList.get(Integer.valueOf(args.get(0)) - 1));
         return CommandResult.success();
     }
 }
